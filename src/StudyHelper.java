2551534e767b3b0745a4be2882299ec0a625e82a
WeilanTao <twlemily@hotmail.com>
2022-04-05 07:35:07 -0400

    styling and positioning complete

--- src/StudyHelper.java
+++ src/StudyHelper.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +16,13 @@ public class StudyHelper extends JFrame{
     private JButton resetButton;
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.
+                    getSystemLookAndFeelClassName());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
         StudyHelper sh = new StudyHelper();
 
         // Layout the elements
@@ -39,40 +47,82 @@ public class StudyHelper extends JFrame{
     }
 
     private static void setLayout (StudyHelper sh){
-        //style
+        //component initializing and styling
+
+        //label
         JLabel title = new JLabel("Good Good Study, Day Day Up", SwingConstants.CENTER);
         sh.clockLabel = new JLabel("Clock Area", SwingConstants.CENTER);
         sh.noticeLabel = new JLabel("Notice Area", SwingConstants.CENTER);
 
-        Font font = title.getFont().deriveFont(32f);
-        title.setFont(font);
-        sh.clockLabel.setFont(font);
-        sh.noticeLabel.setFont(font);
+        Font titleFont = title.getFont().deriveFont(Font.BOLD, 32f);
+        title.setFont(titleFont);
+        title.setVerticalAlignment(JLabel.TOP);
+        sh.clockLabel.setFont(titleFont);
+        sh.noticeLabel.setFont(titleFont);
 
-        sh.restButton = new JButton("Take a Break");
-        sh.studyButton = new JButton("Good Good Study");
-        sh.resetButton = new JButton("Reset Timer");
+        //button
+        sh.restButton = new JButton("Break");
+        sh.studyButton = new JButton("Study");
+        sh.resetButton = new JButton("Reset ");
 
-        Font buttonFount = sh.resetButton.getFont().deriveFont(28f);
+        Font buttonFount = sh.resetButton.getFont().deriveFont(Font.BOLD, 26f);
         sh.restButton.setFont(buttonFount);
         sh.studyButton.setFont(buttonFount);
         sh.resetButton.setFont(buttonFount);
+        sh.restButton.setFocusPainted(false);
+        sh.studyButton.setFocusPainted(false);
+        sh.resetButton.setFocusPainted(false);
+
+
+        int btnw = 200;
+        int btnh = 30;
+        sh.restButton.setPreferredSize(new Dimension(btnw, btnh));
+        sh.studyButton.setPreferredSize(new Dimension(btnw, btnh));
+        sh.resetButton.setPreferredSize(new Dimension(btnw, btnh));
 
         //position
-        sh.panelMain = new JPanel(new GridLayout(3,1,5,5));
-        sh.panelMain.add(title);
+        sh.panelMain = new JPanel(new GridBagLayout());
+        sh.panelMain.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //the title line
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth=3;
+        gbc.gridheight=1;
+        gbc.ipady = 10;
+        gbc.weighty =500;
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        sh.panelMain.add(title,gbc);
+
+        //the clock line
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth=3;
+        gbc.gridheight=1;
+        gbc.ipady = 350;
+        gbc.fill=GridBagConstraints.HORIZONTAL;
         sh.clockPanel= new JPanel(new GridLayout(1,2));
         sh.clockPanel.add(sh.clockLabel);
         sh.clockPanel.add(sh.noticeLabel);
-        sh.panelMain.add(sh.clockPanel);
+        sh.panelMain.add(sh.clockPanel,gbc);
 
-        sh.buttonPanel= new JPanel(new GridLayout(1,3));
-        sh.buttonPanel.add(sh.studyButton);
-        sh.buttonPanel.add(sh.restButton);
-        sh.buttonPanel.add(sh.resetButton);
-        sh.panelMain.add(sh.buttonPanel);
 
+        //the button line
+        sh.buttonPanel= new JPanel(new GridLayout(1,3,50,5));
+        sh.buttonPanel.add(sh.studyButton,gbc);
+        sh.buttonPanel.add(sh.restButton,gbc);
+        sh.buttonPanel.add(sh.resetButton,gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth=2;
+        gbc.gridheight=1;
+        gbc.ipady = btnh;
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        sh.panelMain.add(sh.buttonPanel,gbc);
+
+        //add painMain to JFrame studyHelper
         sh.setContentPane(sh.panelMain);
 
     }
